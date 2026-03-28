10ee7462de2805fbcf3f3c60e08ad233ba8e01e5
Pushpanjay <60690737+Pushpanjay@users.noreply.github.com>
2026-03-29 00:01:38 +0530

    Create UserServiceTest.java

---
+++ src/test/com/devops/service/UserServiceTest.java
@@ -0,0 +1,28 @@
+package com.devops.service;
+
+import com.devops.model.User;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class UserServiceTest {
+
+    UserService service = new UserService();
+
+    @Test
+    public void testValidUser() {
+        User user = new User("John", 25);
+        assertEquals("Valid User", service.validateUser(user));
+    }
+
+    @Test
+    public void testUnderageUser() {
+        User user = new User("John", 15);
+        assertEquals("Underage", service.validateUser(user));
+    }
+
+    @Test
+    public void testInvalidName() {
+        User user = new User("", 25);
+        assertEquals("Invalid Name", service.validateUser(user));
+    }
+}
