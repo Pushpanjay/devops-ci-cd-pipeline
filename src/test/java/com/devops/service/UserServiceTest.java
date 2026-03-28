ee57bc9e6ae7ef095794ea04a722a94d65370ee0
Pushpanjay <60690737+Pushpanjay@users.noreply.github.com>
2026-03-29 02:11:55 +0530

    Update UserServiceTest.java

--- src/test/java/com/devops/service/UserServiceTest.java
+++ src/test/java/com/devops/service/UserServiceTest.java
@@ -1,8 +1,8 @@
 package com.devops.service;
 
 import com.devops.model.User;
-import org.junit.Test;
-import static org.junit.Assert.*;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class UserServiceTest {
 
