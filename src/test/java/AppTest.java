82b27444eadad11fb13f9a5b66efceb0c7a044d7
Pushpanjay <60690737+Pushpanjay@users.noreply.github.com>
2026-03-28 23:29:56 +0530

    Create AppTest.java

---
+++ src/test/java/AppTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class AppTest {
+
+    @Test
+    public void testSample() {
+        int result = 2 + 2;
+        assertEquals(4, result);
+    }
+}
